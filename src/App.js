671b973d2d43f6d751b1fb0e35aa519771c8a279
Alex Sverdlov <sverdlov.alexandr@gmail.com>
2021-02-24 21:49:09 +0200

    ex #1

--- src/App.js
+++ src/App.js
@@ -1,16 +1,37 @@
-import "./App.css";
-import Profile from "./components/Profile/Profile";
-import user from "./db/user.json";
+import './App.css';
+import Profile from './components/Profile/Profile';
+import user from './db/user.json';
 
 function App() {
   return (
-    <Profile
-      name={user.name}
-      tag={user.tag}
-      avatar={user.avatar}
-      location={user.location}
-      stats={user.stats}
-    />
+    <>
+      <div
+        style={{
+          marginTop: '100px',
+        }}
+      >
+        <Profile
+          name={user.name}
+          tag={user.tag}
+          avatar={user.avatar}
+          location={user.location}
+          stats={user.stats}
+        />
+      </div>
+      <div
+        style={{
+          marginTop: '100px',
+        }}
+      >
+        <Profile
+          name={user.name}
+          tag={user.tag}
+          avatar={user.avatar}
+          location={user.location}
+          stats={user.stats}
+        />
+      </div>
+    </>
   );
 }
 
--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -1,29 +1,39 @@
-import React from "react";
+import React from 'react';
+import PropTypes from 'prop-types';
+import styles from './Profile.module.css';
 
 const Profile = ({ name, tag, avatar, location, stats }) => (
-  <div>
+  <div className={styles.profile}>
     <div>
-      <img src={avatar} alt={name} />
-      <p>{name}</p>
-      <p>@{tag}</p>
-      <p>{location}</p>
+      <img className={styles.avatar} src={avatar} alt={name} />
+      <p className={styles.name}>{name}</p>
+      <p className={styles.tag}>@{tag}</p>
+      <p className={styles.location}>{location}</p>
     </div>
-    <ul>
-      <li>
-        <span>Followers</span>
-        <span>{stats.followers}</span>
+    <ul className={styles.stats}>
+      <li className={styles.item}>
+        <span className={styles.label}>Followers</span>
+        <span className={styles.quantity}>{stats.followers}</span>
       </li>
-      <li>
-        <span>Views</span>
-        <span>{stats.views}</span>
+      <li className={styles.item}>
+        <span className={styles.label}>Views</span>
+        <span className={styles.quantity}>{stats.views}</span>
       </li>
-      <li>
-        <span>Likes</span>
-        <span>{stats.likes}</span>
+      <li className={styles.item}>
+        <span className={styles.label}>Likes</span>
+        <span className={styles.quantity}>{stats.likes}</span>
       </li>
     </ul>
   </div>
 );
+
+Profile.propTypes = {
+  name: PropTypes.string,
+  tag: PropTypes.string,
+  avatar: PropTypes.string,
+  location: PropTypes.string,
+  stats: PropTypes.objectOf(PropTypes.number),
+};
 
 export default Profile;
 /* <div class="profile">
